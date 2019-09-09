f157c62720ba5860754e5efc9088baded3b07526
driespieters <hello@driespieters.com>
2019-09-09 16:01:41 +0200

    Hello world

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#0B0B0B`,
       marginBottom: `1.45rem`,
     }}
   >
