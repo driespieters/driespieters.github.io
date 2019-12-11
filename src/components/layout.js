8059e75c09549d9d6c1d5b60114f4cbfcc9fb307
driespieters <hello@driespieters.com>
2019-12-11 10:02:46 +0100

    Sass structure

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,6 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-
-import "./layout.scss"
+import "../styles/main.scss"
 
 const Layout = ({ children }) => {
   return (
