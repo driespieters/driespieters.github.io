933df5b6fc027ad104f20dc6b0e0c3eaa5157dc6
driespieters <hello@driespieters.com>
2020-04-30 10:35:19 +0200

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -18,7 +18,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero__content">
-        <p>I’m <strong>Dries Pieters</strong>, a Brussels based front-end developer. Over the 9 years of my career I've specialized myself in Shopify, helping start-ups and establised brands with their e-commerce strategy.</p>
+        <p>I’m <strong>Dries Pieters</strong>, a Brussels based front-end developer. Over the 9 years of my career I've specialized myself in Shopify, helping start-ups and established brands with their e-commerce strategy.</p>
         <p>If you think I'd be a good fit for your project, please get in touch and we can chat.</p>
         <a href="https://www.shopify.be/?ref=driespieters" target="_blank" title="Shopify Plus Expert" className="hero__shopify" rel="noopener noreferrer">
           <LogoShopify />
