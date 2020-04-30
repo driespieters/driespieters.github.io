7ed865c9e603107c553d92d323f17b821a63cad2
driespieters <hello@driespieters.com>
2020-04-30 22:32:59 +0200

    work

--- src/components/work.js
+++ src/components/work.js
@@ -6,6 +6,10 @@ const Work = props => (
       <h2>Selected work</h2>
     </div>
     <div className="work__content">
+      <div className="workItem">
+        <h3>Gimber</h3>
+        <p>GIMBER is the organic alternative to any alcoholic drink. We packed this concentrate with a.o. the best organic Peruvian ginger, lemons, herbs, spices and gems in order to present your taste buds with a tango.</p>
+      </div>
     </div>
   </section>
 )
