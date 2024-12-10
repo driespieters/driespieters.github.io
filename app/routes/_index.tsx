5b5e0f228ce9b95bf1b645dc0246ba52996888a3
Dries Pieters <hello@driespieters.com>
2024-12-10 21:50:58 +0100

    Change route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,7 +14,9 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <Link to="/about">about</Link>;
+      <div className="block my-24">
+        <Link to="/about">about</Link>
+      </div>
       <Image
         src={spelen}
         alt="A viaduct"
