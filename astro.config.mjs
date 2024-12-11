ddb06610f57c14ab6beb255c62530246ac68e868
Dries Pieters <hello@driespieters.com>
2024-12-11 10:25:23 +0100

    Add tailwind and test image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [tailwind()]
 });
