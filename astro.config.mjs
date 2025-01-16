0aed0d6c28c252129888298607e4b6bcbc2554ce
Dries Pieters <hello@driespieters.com>
2025-01-16 14:28:25 +0100

    Re-add experimental config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://driespieters.github.io",
   integrations: [tailwind()],
+  experimental: {
+    responsiveImages: true,
+    svg: true,
+  },
 });
