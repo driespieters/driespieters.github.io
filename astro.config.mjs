6224380c351841fb478dc81b1b6dcd4ce1b26a50
Dries Pieters <hello@driespieters.com>
2025-04-29 15:27:22 +0200

    update astro tailwind for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: "https://driespieters.com",
